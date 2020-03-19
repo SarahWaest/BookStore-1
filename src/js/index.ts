132d2925dfa8a1d9de1e10c20d895630ef263b79
Nikolaj Dufke <nikolajdufke@live.dk>
2020-03-19 09:35:08 +0100

    Co-authored-by: Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,12 +3,47 @@ import Axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 
+import {json2table100} from "./genericTable";
+
 let BaseUri: string = "http://anbo-bookstorerest.azurewebsites.net/api/"
-
-
-
-
-
-
-
+let contentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("table_content");
+let AllBooks: JSON;
+
+
+
+interface IBook {
+   id: number,
+   title: string,
+   author: string,
+   publisher: string,
+   price: number,
+}
+
+ Axios.get(BaseUri + "Books").then(
+     function(respone: AxiosResponse): void{
+         console.log("getting books... v4");
+         element.innerHTML = "<div class='spinner-border text-muted></div>";
+         setTimeout(() => {  console.log("waited 5 sek"); }, 5000);
+    let data: IBook[] = respone.data;
+        console.log(data);
+    let result: string = json2table100(data);
+console.log(result);
+    element.innerHTML = result;
+        contentElement.innerHTML = JSON.stringify(respone.data);
+     }
+ )
+ .catch(
+     function(error: AxiosError): void{
+        contentElement.innerHTML = error.message;
+     }
+ )
+
+
+
+
+ 
+
+
+
+
